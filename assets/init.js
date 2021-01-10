6353f4658aeaca83e1ca0a857b6e12e32e256d92
Abdur Rahim <rahimbd623@gmail.com>
2021-01-10 23:50:50 +0600

    update all info in 2021

--- assets/init.js
+++ assets/init.js
@@ -515,7 +515,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Freelancer", "UI/UX Designer", "Web Developer"],
+			strings: ["Full-Stack Software Engineer", "UI/UX Designer", "Android App Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
